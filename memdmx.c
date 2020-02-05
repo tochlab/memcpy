4f4c7bef4cc887374dedc18782f2290a628edb9a
Dmitrii Tochanskii <tochansky@tochlab.net>
2020-02-05 11:17:23 +0300

    Pure memcpy

--- memdmx.c
+++ memdmx.c
@@ -3,56 +3,49 @@
 #include <sys/time.h>
 #include <malloc.h>
 
-typedef struct _iqs_ {
-    short i;
-    short q;
-} iqs;
-
-void demux(iqs *src, iqs *dst0, iqs *dst1, int len) {
-    for(int i = 0, j=0;i<len;i+=2,j++) {
-        dst0[j] = src[i];
-        dst1[j] = src[i+1];
-    }
-}
+#define TEST_TYPE int
 
 inline unsigned long get_mks() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return tv.tv_sec*1000000 + tv.tv_usec;
+    return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+void memcpy_test(const TEST_TYPE* src, TEST_TYPE* dst, int len) {
+    for (int i = 0; i < len; i++) {
+        dst[i] = src[i];
+    }
 }
 
 int main() {
-    int len;
-    if (!fscanf(stdin, "%i", &len) ) {
-        return 0;
-    };
-    iqs *src = malloc(len*sizeof(iqs));
-    iqs *dst0 = malloc(len/2*sizeof(iqs));
-    iqs *dst1 = malloc(len/2*sizeof(iqs));
-    srand(len);
-    for(int i = 0;i<len;i++) {
-        src[i].i = rand() % 2 + 1;
-        src[i].q = rand() % 2 + 1;
-    }
-    unsigned long start_mks = get_mks();
-    demux(src, dst0, dst1, len);
-    unsigned long end_mks = get_mks();
-    unsigned long mks = end_mks-start_mks;
+    int len = 128 * 1024 * 1024;
+    printf("Allocating %lu bytes\n", len * sizeof(TEST_TYPE));
+    TEST_TYPE* src = malloc(len * sizeof(TEST_TYPE));
+    TEST_TYPE* dst = malloc(len * sizeof(TEST_TYPE));
 
-    double MBytes = (((double) len*2*sizeof(iqs)) / (1024*1024.0f));
+    srand(len);
+    for (int i = 0; i < len; i++) {
+        src[i] = rand();
+    }
+
+    unsigned long start_mks = get_mks();
+    memcpy_test(src, dst, len);
+    unsigned long end_mks = get_mks();
+
+    unsigned long mks = end_mks - start_mks;
+    double MBytes = (((double) len * sizeof(TEST_TYPE)) / (1024 * 1024.0f));
     double secs = mks / 1000000.0f;
-    printf("DemuxTime %lu size %f speed %f Mb/s\n", mks, MBytes, MBytes/secs);
+
+    printf("Time %lu mks size %.2f MBytes speed %.2f MB/s\n", mks, MBytes, MBytes / secs);
 
     int sum = 0;
-    for(int i = 0;i<len/2;i++) {
-        sum += dst0[i].i;
-        sum += dst1[i].q;
-        sum -= src[i].q;
+    for (int i = 0; i < len; i++) {
+        sum += dst[i];
     }
-    free(dst0);
-    free(dst1);
-    free(src);
-    printf("Random sum %d\n", sum);
 
+    printf("Sum %d\n", sum);
+
+    free(src);
+    free(dst);
     return 0;
 }
